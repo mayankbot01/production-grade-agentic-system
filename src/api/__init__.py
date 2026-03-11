c85e6284ad035938790835a130595d37a5669d67
mayankbot01 <mayankbot2003@gmail.com>
2026-03-11 22:14:26 +0530

    Create __init__.py

---
+++ src/api/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from .routes import auth, chat
+
+api_router = APIRouter()
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+
+__all__ = ["api_router"]
